66a1b1933aef8c4cfbd9306b4ffed60dce318034
Gabriel Agles Gomes <gabriel.aglesss@gmail.com>
2025-10-14 21:07:45 -0300

    Criação da página principal

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuToggle = document.getElementById('menu-toggle');
+const navbar = document.querySelector('.navbar');
+
+menuToggle.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+    menuToggle.classList.toggle('open');
+});
